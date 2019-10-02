1592b2d7d9596d032a2be2eb61cfa2a5daf7552b
Vitaly <vitalian42@mail.ru>
2019-10-02 16:43:19 +0300

    CORS fix

--- simple-server.go
+++ simple-server.go
@@ -656,6 +656,8 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Content-Type", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Origin", "http://solar-env.v2zxh2s3me.us-east-2.elasticbeanstalk.com")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
 	})
 }
