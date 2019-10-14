121dbf8e7ed4c8c83aa7be4daf083afb87d67b09
veinskiyVA <veinskiyVA@mail.ru>
2019-10-14 11:35:54 +0300

    merge pkg

---
+++ simple-server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"2019_2_Solar/pinterest/delivery"
+	"2019_2_Solar/pkg/middlewares"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleRoot)))
+	http.Handle("/users/", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleUsers)))
+	http.Handle("/registration/", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleRegistration)))
+	http.Handle("/login/", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleLogin)))
+	http.Handle("/logout/", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleLogout)))
+	http.Handle("/profile/data", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleProfileData)))
+	http.Handle("/profile/picture", middlewares.CORSMiddleware(http.HandlerFunc(delivery.HandleProfilePicture)))
+
+	http.ListenAndServe(":8080", nil)
+}
