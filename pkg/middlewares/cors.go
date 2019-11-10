58fd7bb4d06d2317b48a14a94c58c1e9d787b529
Bogdan Salyp <bogdansalyp@gmail.com>
2019-11-10 23:09:38 +0300

    Switched to the DigitalOcean

--- pkg/middlewares/cors.go
+++ pkg/middlewares/cors.go
@@ -9,7 +9,7 @@ func CORSMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		ctx.Response().Header().Set("Content-Type", "*")
 		ctx.Response().Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		ctx.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		ctx.Response().Header().Set("Access-Control-Allow-Origin", "http://solar-env.v2zxh2s3me.us-east-2.elasticbeanstalk.com")
+		ctx.Response().Header().Set("Access-Control-Allow-Origin", "http://167.172.183.157:3000")
 		ctx.Response().Header().Set("Access-Control-Allow-Credentials", "true")
 		if ctx.Request().Method == "OPTIONS" {
 			return nil
