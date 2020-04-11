882a4533fbb9b213d6fdca32f2455c5efc28174b
BarniBl <veinskiyva@mail.ru>
2020-04-11 14:30:12 +0300

    fixed

--- pkg/middlewares/cors.go
+++ pkg/middlewares/cors.go
@@ -9,7 +9,7 @@ func (MS *MiddlewareStruct) CORSMiddleware(next echo.HandlerFunc) echo.HandlerFu
 		ctx.Response().Header().Set("Content-Type", "*")
 		ctx.Response().Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		ctx.Response().Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, csrf-token, Authorization")
-		ctx.Response().Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		ctx.Response().Header().Set("Access-Control-Allow-Origin", "https://solarsunrise.ru")
 		ctx.Response().Header().Set("Access-Control-Allow-Credentials", "true")
 		if ctx.Request().Method == "OPTIONS" {
 			return nil
